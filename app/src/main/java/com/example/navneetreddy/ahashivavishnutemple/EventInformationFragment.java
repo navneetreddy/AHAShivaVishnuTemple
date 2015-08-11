5a2d0f2aa8553ccf75b39d128c859ef43047d48b
Navneet Reddy <navneet@tds.net>
2015-08-10 21:37:26 -0500

    Event Information Fragment - SaveToGoogleCalendar() in the works.

--- app/src/main/java/com/example/navneetreddy/ahashivavishnutemple/EventInformationFragment.java
+++ app/src/main/java/com/example/navneetreddy/ahashivavishnutemple/EventInformationFragment.java
@@ -1,20 +1,61 @@
 package com.example.navneetreddy.ahashivavishnutemple;
 
+import android.accounts.AccountManager;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.provider.CalendarContract;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.common.AccountPicker;
+import com.squareup.picasso.Picasso;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class EventInformationFragment extends Fragment {
+
+    /**
+     * Public fields required for Google Calendar
+     */
+    public static final String[] EVENT_PROJECTION = new String[] {
+            CalendarContract.Calendars._ID,                           // 0
+            CalendarContract.Calendars.ACCOUNT_NAME,                  // 1
+            CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,         // 2
+            CalendarContract.Calendars.OWNER_ACCOUNT                  // 3
+    };
+
+    // The indices for the projection array above.
+    private static final int PROJECTION_ID_INDEX = 0;
+    private static final int PROJECTION_ACCOUNT_NAME_INDEX = 1;
+    private static final int PROJECTION_DISPLAY_NAME_INDEX = 2;
+    private static final int PROJECTION_OWNER_ACCOUNT_INDEX = 3;
+
+    private static final int REQUEST_CODE_EMAIL = 1;
+
+    private String[] calendarInfo;
+    private Calendar startDateCalendar;
+    private Calendar endDateCalendar;
+
+    private String userEmail;
 
     private Event event;
 
@@ -25,13 +66,15 @@ public class EventInformationFragment extends Fragment {
     private TextView contactPhoneText;
     private TextView contactEmailText;
 
+    private ImageButton callButton;
+    private ImageButton emailButton;
+
     private Button viewEventDetailsButton;
     private Button addToGoogleCalendarButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_event_information, container, false);
     }
 
@@ -54,8 +97,14 @@ public class EventInformationFragment extends Fragment {
         contactPhoneText = (TextView) view.findViewById(R.id.eventContactPhone);
         contactEmailText = (TextView) view.findViewById(R.id.eventContactEmail);
 
+        callButton = (ImageButton) view.findViewById(R.id.eventContactCallButton);
+        emailButton = (ImageButton) view.findViewById(R.id.eventContactEmailButton);
+
         viewEventDetailsButton = (Button) view.findViewById(R.id.viewEventDetailsButton);
         addToGoogleCalendarButton = (Button) view.findViewById(R.id.addToGoogleCalendarButton);
+
+        startDateCalendar = Calendar.getInstance();
+        endDateCalendar = Calendar.getInstance();
     }
 
     private void setFields() {
@@ -65,6 +114,35 @@ public class EventInformationFragment extends Fragment {
         contactNameText.setText(event.getContactName());
         contactPhoneText.setText(event.getContactPhone());
         contactEmailText.setText(event.getContactEmail());
+
+        Picasso.with(getActivity())
+                .load(R.drawable.ic_call_black_24dp)
+                .fit()
+                .centerInside()
+                .into(callButton);
+
+        Picasso.with(getActivity())
+                .load(R.drawable.ic_email_black_24dp)
+                .fit()
+                .centerInside()
+                .into(emailButton);
+
+        try {
+            Intent intent = AccountPicker.newChooseAccountIntent(null, null,
+                    new String[] {GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE},
+                    false, null, null, null, null);
+            startActivityForResult(intent, REQUEST_CODE_EMAIL);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_EMAIL && resultCode == Activity.RESULT_OK) {
+            userEmail = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+            calendarInfo = getCalendar();
+        }
     }
 
     private void setClickListeners() {
@@ -76,6 +154,20 @@ public class EventInformationFragment extends Fragment {
         });
 
         contactEmailText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        emailButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -96,8 +188,77 @@ public class EventInformationFragment extends Fragment {
         addToGoogleCalendarButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                saveToGoogleCalendar();
             }
         });
+    }
+
+    private String[] getCalendar() {
+        Cursor cur;
+        ContentResolver cr = getActivity().getContentResolver();
+
+        Uri uri = CalendarContract.Calendars.CONTENT_URI;
+
+        String selection = "((" + CalendarContract.Calendars.ACCOUNT_NAME + " = ?) AND ("
+                + CalendarContract.Calendars.ACCOUNT_TYPE + " = ?) AND ("
+                + CalendarContract.Calendars.OWNER_ACCOUNT + " = ?))";
+
+        String[] selectionArgs = new String[] {userEmail , "com.google", userEmail};
+
+        cur = cr.query(uri, EVENT_PROJECTION, selection, selectionArgs, null);
+
+        long calID = 0;
+        String displayName = null;
+        String accountName = null;
+        String ownerName = null;
+
+        while (cur.moveToNext()) {
+            calID = cur.getLong(PROJECTION_ID_INDEX);
+            displayName = cur.getString(PROJECTION_DISPLAY_NAME_INDEX);
+            accountName = cur.getString(PROJECTION_ACCOUNT_NAME_INDEX);
+            ownerName = cur.getString(PROJECTION_OWNER_ACCOUNT_INDEX);
+        }
+
+        cur.close();
+
+        return new String[] {String.valueOf(calID), displayName, accountName, ownerName};
+    }
+
+    private void saveToGoogleCalendar() {
+        String[] dateArray = event.getDate().split("/");
+        String[] timeArray = event.getTime().split("(:)|(\\s)");
+
+
+//        Date startDate = startDateCalendar.getTime();
+//        Date endDate = endDateCalendar.getTime();
+//        String comments = comments_entry.getText().toString();
+//        String title = meeting_entry.getText().toString();
+
+        /**
+         * SAVE TO GOOGLE CALENDAR
+         */
+        long calID = Long.parseLong(calendarInfo[PROJECTION_ID_INDEX]);
+
+        ContentResolver cr = getActivity().getContentResolver();
+        ContentValues values = new ContentValues();
+
+//        values.put(CalendarContract.Events.DTSTART, startDate.getTime());
+//        values.put(CalendarContract.Events.DTEND, endDate.getTime());
+//        values.put(CalendarContract.Events.TITLE, title);
+//        values.put(CalendarContract.Events.DESCRIPTION, comments);
+        values.put(CalendarContract.Events.CALENDAR_ID, calID);
+        values.put(CalendarContract.Events.EVENT_TIMEZONE, TimeZone.getDefault().getID());
+
+        Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
+
+        long eventID = Long.parseLong(uri.getLastPathSegment());
+
+        if(eventID != -1) {
+            Toast.makeText(getActivity(), "Appointment saved to Google Calendar",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getActivity(), "Appointment not saved to Google Calendar",
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
